9f0b39685b4f74dcd8b71c47cdc92cd5ae037fcb
convinto <convinto@yandex.ru>
2024-09-07 22:56:41 +0300

    Update module_8_1.py

--- module_8_1.py
+++ module_8_1.py
@@ -3,9 +3,6 @@ def add_everything_up(a, b):
         return a + b
     except TypeError:
         return str(a) + str(b)
-
-# Примеры использования
-
 print(add_everything_up(123.456, 'строка'))
 print(add_everything_up('яблоко', 4215))
 print(add_everything_up(123.456, 7))
